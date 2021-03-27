09de595583a2b9cddc091fad7904052b9ded91f9
Rinaldo <Rinaldo.nelson1@gmail.com>
2021-03-27 13:14:40 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+
+//create an Express Application Object 
+
+const app = express();
+
+//variable ^
+
+//create webserver that will listen to a specific port
+
+const PORT = 3000;
+
+app.listen(PORT, ()=>{
+
+    con
